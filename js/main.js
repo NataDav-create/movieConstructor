63ed1c6f4c0c0382c0e30d4b214058e632eaded8
Natalia Davydova <davydovanatalia063@gmail.com>
2021-07-20 23:26:38 +0400

    add social links

--- js/main.js
+++ js/main.js
@@ -22,21 +22,59 @@
 // 	menu.classList.toggle('header-active');
 // })
 
-const getElement = (tagName) => {
+const getElement = (tagName, classNames, attributes) => {
 	const element = document.createElement(tagName);
+	if (classNames) {
+		element.classList.add(...classNames)
+	}
+	if (attributes) {
+		for (const attribute in attributes) {
+			element[attribute] = attributes[attribute]
+		}
+	}
 	return element;
 }
 
-const createHeader = () => {
+const createHeader = (param) => {
 	const header = getElement('header');
+	const container = getElement('div', ['container']);
+	const wrapper = getElement('div', ['header']);
+
+	if (param.header.logo) {
+		const logo = getElement('img', ['logo'], {
+			src: param.header.logo,
+			alt: `Logo ${param.title}`,
+		});
+		wrapper.append(logo)
+	}
+
+	if (param.header.social) {
+		const socialWrapper = getElement('div', ['social']);
+		const allSocial = param.header.social.map(item => {
+			const socialLink = getElement('a', ['social-link']);
+			socialLink.append(getElement('img', [], {
+				src: item.image,
+				alt: item.title
+			}));
+			socialLink.href = item.link;
+			return socialLink
+		});
+		console.log(allSocial)
+		socialWrapper.append(...allSocial);
+		wrapper.append(socialWrapper)
+	}
+
+	header.append(container);
+	container.append(wrapper);
+
 	return header
 };
 
 const movieConstructor = (selector, options) => {
 	const app = document.querySelector(selector);
+	app.classList.add('body-app');
 	if (options.header) {
-		const header = createHeader();
-		app.append(header);
+		app.append(createHeader(options));
 	}
 
 };
@@ -44,6 +82,22 @@ const movieConstructor = (selector, options) => {
 movieConstructor('.app', {
 	title: 'Witcher',
 	header: {
-		logo: '',
+		logo: 'witcher/logo.png',
+		social: [{
+				title: 'Twitter',
+				link: 'https://twitter.com',
+				image: 'witcher/social/twitter.svg'
+			},
+			{
+				title: 'Instagramm',
+				link: 'https://instagram.com',
+				image: 'witcher/social/instagram.svg'
+			},
+			{
+				title: 'Facebook',
+				link: 'https://facebook.com',
+				image: 'witcher/social/facebook.svg'
+			}
+		]
 	}
 })
